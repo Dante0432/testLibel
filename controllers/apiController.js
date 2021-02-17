06b14d54d4cdabed3ca492de2a8b91585bbf13a7
Gabo <jgcardenasa@gmail.com>
2021-02-17 12:03:58 -0500

    Generación y validación de jwt

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,10 +1,11 @@
 const { catchAsync } = require("../utils/miscellany")
 const bcrypt = require('bcrypt');
-//const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
 const User = require("../models/User")
 
 exports.getActiveUsers = catchAsync(async (req,res,next) => {
+  console.log('entra')
   const users = await User.find({status:true})
   res.status(200).json({
     status: "success",
@@ -12,9 +13,19 @@ exports.getActiveUsers = catchAsync(async (req,res,next) => {
   })
 })
 exports.login = catchAsync(async (req,res,next) => {
+  const user = await User.findOne({ email: req.body.email })
+  
+  const validPassword = await bcrypt.compare(req.body.password, user.password)
+  if (!validPassword||!user) throw new Error('Invalid username or password')
+    // create token
+    const token = jwt.sign({
+      name: user.name,
+      id: user._id
+    }, process.env.TOKEN_SECRET)
+  
   res.status(200).json({
     status: "success",
-    token: "token"
+    token
   })
 })
 
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
 const apiController = require("./../controllers/apiController")
+const { verifyToken } = require("../utils/miscellany")
 
-router.get("/users", apiController.getActiveUsers)
+router.get("/users", verifyToken, apiController.getActiveUsers)
 router.post("/login", apiController.login)
 router.post("/createUser", apiController.createUser)
 
